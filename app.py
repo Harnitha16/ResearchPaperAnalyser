14a4bca9ff9fef986d670163200d0fae831a88b6
Harnitha16 <harnitha.suresh1999@gmail.com>
2024-04-16 05:38:41 +0000

    deprecated replacement

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ def extract_text_from_pdf(pdf_file):
     text = ""
     RP_file = save_uploaded_file(pdf_file)
     with open(RP_file, "rb") as file:
-        pdf_reader = PyPDF2.PdfFileReader(file)
-        num_pages = pdf_reader.numPages
+        pdf_reader = PyPDF2.PdfReader(file)
+        num_pages = len(pdf_reader.pages)
         for page_num in range(num_pages):
-            page = pdf_reader.getPage(page_num)
-            text += page.extractText()
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
     return text
 
 
